4d9470de11072f7db8bd0ee6470287e9454e333a
Muhwezi Aggrey <aggrey256@gmail.com>
2022-01-16 22:07:38 +0300

    add unit tests and logic

--- src/customers/controller.ts
+++ src/customers/controller.ts
@@ -17,8 +17,7 @@ export const calculateAPY = (req: Request, res: Response) => {
     const amountPayable = deposit + deposit * Number(APY);
 
     const query = db.prepare(
-        'insert into calculations(customerId, yearlyCompoundTimes, interestRate, deposit, apy, total)' +
-            'values (?,?,?,?,?,?)',
+        `insert into calculations (customerId, yearlyCompoundTimes, interestRate, deposit, apy, total) values (?,?,?,?,?,?)`,
         [
             customerId,
             yearlyCompoundedTimes,
@@ -30,13 +29,11 @@ export const calculateAPY = (req: Request, res: Response) => {
     );
     return query.run((error) => {
         if (error) {
-            return res
-                .status(500)
-                .send({
-                    message: 'Something went wrong',
-                    success: false,
-                    error,
-                });
+            return res.status(500).send({
+                message: 'Something went wrong',
+                success: false,
+                error,
+            });
         }
         return res.status(201).send({
             data: {
@@ -52,24 +49,26 @@ export const calculateAPY = (req: Request, res: Response) => {
     });
 };
 
+export const retrieveCustomerCalculations = (req: Request, res: Response) => {
+    const { customerId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
+    const offset = Number(limit) * (Number(page) - 1);
+    let sql = `SELECT * FROM calculations WHERE customerId = ? LIMIT ${limit} OFFSET ${offset}`;
 
-// export const retrieveCustomerCalculations = (req: Request, res: Response) => {
-//     const {customerId} = req.query
+    return db.all(sql, [customerId], (error, result) => {
+        if (error) {
+            return res.status(500).send({
+                message: 'Something went wrong',
+                success: false,
+                error,
+                customerId,
+            });
+        }
 
-//     let sql = `SELECT customerId total id,
-//            FROM calculations
-//            WHERE customerId  = ?`;
-//     return db.get(sql, [customerId], (error, result) => {
-//         if (error) {
-//             return res
-//                 .status(500)
-//                 .send({
-//                     message: 'Something went wrong',
-//                     success: false,
-//                     error,
-//                 });
-//         }
-
-//         return res.send({data: result});
-//     })
-// }
+        return res.send({
+            data: result,
+            nextPage: result.length === Number(limit) ? Number(page) + 1 : null,
+            currentPage: page,
+        });
+    });
+};
